2cd5416363669feb764eb56a08cad27eebe495b4
kimgeon0802 <107537106+kimgeon0802@users.noreply.github.com>
2026-02-25 14:29:27 +0900

    Update F_recommendations.py

--- database_script/F_recommendations.py
+++ database_script/F_recommendations.py
@@ -1,31 +1,42 @@
-import pandas as pd
-import json
-import os
-import glob
 import pymysql
-from sqlalchemy import create_engine
+import pandas as pd
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy.dialects.mysql import insert
+from datetime import datetime
+import os
 
+# MySQL 연동
 pymysql.install_as_MySQLdb()
 
-json_files = glob.glob(os.path.join('data', 'recommendations_*.json'))
-if not json_files:
-    print("데이터를 찾을 수 없습니다: recommendations_*.json")
-    exit()
+# 오늘 날짜 YYYYMMDD 형식
+today_str = datetime.now().strftime("%Y%m%d")
 
-latest_file = max(json_files, key=os.path.getctime)
-with open(latest_file, 'r', encoding='utf-8') as f:
-    data = json.load(f)
+# 파일명 생성
+file_name = f"recommendations_signals_{today_str}.csv"
+#file_name = f"recommendations_20260224.csv" #임시 테스트
+csv_file_path = f"data/{file_name}"
+df = pd.read_csv(csv_file_path, encoding="utf-8")
+recommendations_df = df[["user_id", "ticker", "as_of","score","reason"]].copy()
 
-if isinstance(data, dict):
-    key = list(data.keys())[0]
-    table_df = pd.DataFrame(data[key])
-else:
-    table_df = pd.DataFrame(data)
+recommendations_df["ticker"] = recommendations_df["ticker"].astype(str)
 
-engine = create_engine('mysql+pymysql://test:test@25.4.53.12:3306/stock_db?charset=utf8mb4')
+engine = create_engine(
+    "mysql+pymysql://test:test@25.4.53.12:3306/stock_db?charset=utf8mb4"
+)
+
+metadata = MetaData()
+metadata.reflect(bind=engine)
+
+recommendations_table = Table("recommendations", metadata, autoload_with=engine)
+
 try:
     with engine.begin() as conn:
-        table_df.to_sql(name='recommendations', con=conn, if_exists='replace', index=False)
-    print("F_recommendations 테이블 데이터 교체/추가 완료")
+        for _, row in recommendations_df.iterrows():
+            stmt = insert(recommendations_table).values(**row.to_dict())
+            stmt = stmt.prefix_with("IGNORE")  # 중복 무시
+            conn.execute(stmt)
+
+    print("✅ 중복 제외하고 추천 결과 추가 완료")
+
 finally:
     engine.dispose()
