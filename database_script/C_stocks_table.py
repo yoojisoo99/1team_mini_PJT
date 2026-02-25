d604b5410a89b17afed20169229cc190b1fb6fbf
kimgeon0802 <107537106+kimgeon0802@users.noreply.github.com>
2026-02-25 14:28:32 +0900

    Update C_stocks_table.py

--- database_script/C_stocks_table.py
+++ database_script/C_stocks_table.py
@@ -1,31 +1,48 @@
-import pandas as pd
-import json
-import os
-import glob
 import pymysql
-from sqlalchemy import create_engine
+import pandas as pd
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy.dialects.mysql import insert
+from datetime import datetime
+import os
 
+# MySQL 연동
 pymysql.install_as_MySQLdb()
 
-json_files = glob.glob(os.path.join('data', 'stocks_*.json'))
-if not json_files:
-    print("데이터를 찾을 수 없습니다: stocks_*.json")
-    exit()
+# 오늘 날짜 YYYYMMDD 형식
+today_str = datetime.now().strftime("%Y%m%d")
 
-latest_file = max(json_files, key=os.path.getctime)
-with open(latest_file, 'r', encoding='utf-8') as f:
-    data = json.load(f)
+# 파일명 생성
+file_name = f"stock_data_{today_str}.csv"
+#file_name = f"stock_data_20260224.csv" #임시 테스트
+csv_file_path = f"data/{file_name}"
+df = pd.read_csv(csv_file_path, encoding="utf-8")
+stocks_df = df[["종목코드", "종목명", "시장"]].copy()
 
-if isinstance(data, dict):
-    key = list(data.keys())[0] # "stocks"
-    table_df = pd.DataFrame(data[key])
-else:
-    table_df = pd.DataFrame(data)
+stocks_df.rename(columns={
+    "종목코드": "ticker",
+    "종목명": "name",
+    "시장": "market"
+}, inplace=True)
 
-engine = create_engine('mysql+pymysql://test:test@25.4.53.12:3306/stock_db?charset=utf8mb4')
+stocks_df["ticker"] = stocks_df["ticker"].astype(str)
+
+engine = create_engine(
+    "mysql+pymysql://test:test@25.4.53.12:3306/stock_db?charset=utf8mb4"
+)
+
+metadata = MetaData()
+metadata.reflect(bind=engine)
+
+stocks_table = Table("stocks", metadata, autoload_with=engine)
+
 try:
     with engine.begin() as conn:
-        table_df.to_sql(name='stocks', con=conn, if_exists='replace', index=False)
-    print("C_stocks_table 테이블 데이터 동기화 완료")
+        for _, row in stocks_df.iterrows():
+            stmt = insert(stocks_table).values(**row.to_dict())
+            stmt = stmt.prefix_with("IGNORE")  # 중복 무시
+            conn.execute(stmt)
+
+    print("✅ 중복 제외하고 stocks 데이터 추가 완료")
+
 finally:
     engine.dispose()
