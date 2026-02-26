ac3fb0022aafd3ccd7d35e0d10f3b2fe143b41dd
kimgeon0802 <107537106+kimgeon0802@users.noreply.github.com>
2026-02-26 16:41:06 +0900

    json → csv 확장자 변경

--- database_script/A_users_table.py
+++ database_script/A_users_table.py
@@ -1,30 +1,39 @@
 import pymysql
 import pandas as pd
-import json
-
 from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.dialects.mysql import insert
+from datetime import datetime
+import os
 
+# MySQL 연동
 pymysql.install_as_MySQLdb()
 
-table_df = pd.read_csv('data/users_db.csv')
+# 파일명 생성
+file_name = f"users_db.csv"
+#file_name = f"stock_data_20260224.csv" #임시 테스트
+csv_file_path = f"data/{file_name}"
+df = pd.read_csv(csv_file_path, encoding="utf-8")
+users_df = df[["user_id", "user_password", "user_email"]].copy()
+
+users_df["user_id"] = users_df["user_id"].astype(str)
 
 engine = create_engine(
-    'mysql+pymysql://test:test@25.4.53.12:3306/stock_db?charset=utf8mb4'
+    "mysql+pymysql://test:test@25.4.53.12:3306/stock_db?charset=utf8mb4"
 )
 
 metadata = MetaData()
 metadata.reflect(bind=engine)
-user_table = Table('users', metadata, autoload_with=engine)
+
+users_table = Table("users", metadata, autoload_with=engine)
 
 try:
     with engine.begin() as conn:
-        for _, row in table_df.iterrows():
-            stmt = insert(user_table).values(**row.to_dict())
-            stmt = stmt.prefix_with("IGNORE")
+        for _, row in users_df.iterrows():
+            stmt = insert(users_table).values(**row.to_dict())
+            stmt = stmt.prefix_with("IGNORE")  # 중복 무시
             conn.execute(stmt)
 
-    print("중복 제외하고 유저 데이터 추가 완료")
+    print("✅ 중복 제외하고 유저 데이터 추가 완료")
 
 finally:
     engine.dispose()
