1031638d12504feaf74da182884d6d972578d552
kimgeon0802 <107537106+kimgeon0802@users.noreply.github.com>
2026-02-26 14:34:07 +0900

    저장 경로 수정

--- outbound/A_export_users.py
+++ outbound/A_export_users.py
@@ -4,7 +4,7 @@ ENGINE_URL = "mysql+pymysql://test:test@25.4.53.12:3306/stock_db?charset=utf8mb4
 
 TABLE_NAME = "users"
 ROOT_KEY = "users"
-OUTPUT_PATH = "out_data/users_export.json"
+OUTPUT_PATH = "../out_data/users_export.json"
 
 
 COLUMNS = ["user_id","user_password", "user_email"]  
